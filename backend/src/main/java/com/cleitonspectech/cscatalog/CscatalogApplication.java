9c4c11e56b877f7919e65aae7ff3635747408258
realmeet <cleitonitalorocha@hotmail.com>
2023-02-16 21:45:27 -0300

    Backend project created

---
+++ backend/src/main/java/com/cleitonspectech/cscatalog/CscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.cleitonspectech.cscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CscatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cleitonspectech/cscatalog/CscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleitonspectech.cscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
